97fdf206fd0070dc2a9679ebf0d09f98d60da818
superpromiser <superpromiser@gmail.com>
2022-04-11 21:08:49 -0700

    update header and footer

--- src/Router/index.js
+++ src/Router/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import {Header} from '../Components/Header';
+import {Footer} from '../Components/Footer'
 import { Welcome } from "../Pages/Welcome";
 import { Home } from "../Pages/Home";
 
 export const Router = () => {
     return (
+        <>
+        <Header />
         <Routes>
             <Route path="/" element="Welcome" />
             <Route path="/home" element="Home" />
         </Routes>
+        <Footer />
+        </>
     )
 }
